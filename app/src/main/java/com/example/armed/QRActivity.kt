c996c43b4437444870fdf56b493a992792a16aa5
Extaleus <streetworkoutinlenino@gmail.com>
2024-02-26 23:39:12 +0300

    ARMed 26.02.24

--- app/src/main/java/com/example/armed/QRActivity.kt
+++ app/src/main/java/com/example/armed/QRActivity.kt
@@ -35,7 +35,6 @@ import kotlinx.coroutines.launch
 class QRActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_qr)
         setContent {
             ARSceneViewComposeTheme {
                 // A surface container using the 'background' color from the theme
